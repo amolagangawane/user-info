79efcce3d151f90175cf6b525b214c75c5291547
amol <amolag@maveric-systems.com>
2024-01-08 13:13:49 +0530

    add users controller

---
+++ src/main/java/com/userinfo/repo/UserRepo.java
@@ -0,0 +1,9 @@
+package com.userinfo.repo;
+
+import com.userinfo.model.UserData;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<UserData,Long> {
+}
