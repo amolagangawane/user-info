a7323a8e8afa238d5c1458752577615a337796ca
amolgangawane1992 <amolgangawane007@gmail.com>
2024-01-08 18:32:23 +0530

    remove db part

--- src/main/java/com/userinfo/controller/UserController.java
+++ src/main/java/com/userinfo/controller/UserController.java
@@ -1,45 +1,45 @@
-package com.userinfo.controller;
-
-import com.userinfo.model.UserData;
-import com.userinfo.repo.UserRepo;
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("/users")
-public class UserController {
-
-@Autowired
-private UserRepo userRepo;
-    @GetMapping("/")
-    public ResponseEntity<List<UserData>> getAllUsers(){
-
-        return ResponseEntity.ok(userRepo.findAll());
-    }
-
-    @PostMapping("/add")
-    public  ResponseEntity addUser(HttpServletRequest request, @RequestBody UserData userData){
-
-        return ResponseEntity.ok(userRepo.save(userData));
-    }
-
-    @PutMapping("/update")
-    public ResponseEntity updateUser(HttpServletRequest request, @RequestBody UserData userData){
-
-        return ResponseEntity.ok(userRepo.save(userData));
-    }
-
-    @DeleteMapping("/delete")
-    public ResponseEntity<String> deleteUser(HttpServletRequest request,@PathVariable long userId){
-
-        userRepo.deleteById(userId);
-        return ResponseEntity.ok("user deleted successfully!");
-    }
-
-
-}
+//package com.userinfo.controller;
+//
+//import com.userinfo.model.UserData;
+//import com.userinfo.repo.UserRepo;
+//import jakarta.servlet.http.HttpServletRequest;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("/users")
+//public class UserController {
+//
+//@Autowired
+//private UserRepo userRepo;
+//    @GetMapping("/")
+//    public ResponseEntity<List<UserData>> getAllUsers(){
+//
+//        return ResponseEntity.ok(userRepo.findAll());
+//    }
+//
+//    @PostMapping("/add")
+//    public  ResponseEntity addUser(HttpServletRequest request, @RequestBody UserData userData){
+//
+//        return ResponseEntity.ok(userRepo.save(userData));
+//    }
+//
+//    @PutMapping("/update")
+//    public ResponseEntity updateUser(HttpServletRequest request, @RequestBody UserData userData){
+//
+//        return ResponseEntity.ok(userRepo.save(userData));
+//    }
+//
+//    @DeleteMapping("/delete")
+//    public ResponseEntity<String> deleteUser(HttpServletRequest request,@PathVariable long userId){
+//
+//        userRepo.deleteById(userId);
+//        return ResponseEntity.ok("user deleted successfully!");
+//    }
+//
+//
+//}
