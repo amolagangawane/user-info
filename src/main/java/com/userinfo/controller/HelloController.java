4a2add3b0dfa54a59f4a500b56a9ab483aa3f063
amolgangawane1992 <amolgangawane007@gmail.com>
2024-01-09 12:55:43 +0530

    req updated

--- src/main/java/com/userinfo/controller/HelloController.java
+++ src/main/java/com/userinfo/controller/HelloController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/")
-    public String sayHello(HttpServletRequest request){
+    public String sayHello(){
 
         return "welcome!";
     }
