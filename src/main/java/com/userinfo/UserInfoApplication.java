4c30a9f9b971ae76e6d88d257f90a6aaab2cde06
amolgangawane1992 <amolgangawane007@gmail.com>
2024-01-09 16:27:44 +0530

    to deploy on external tomcat

--- src/main/java/com/userinfo/UserInfoApplication.java
+++ src/main/java/com/userinfo/UserInfoApplication.java
@@ -2,12 +2,18 @@ package com.userinfo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UserInfoApplication {
+public class UserInfoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserInfoApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(UserInfoApplication.class);
+	}
 }
